66fc580a15033b52634b4f220204dcdcedf1efc0
Cesar Paublini <cesar.paublini001@gmail.com>
2025-08-05 11:07:07 -0400

    Installed vecel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/app/globals.css";
 import Navbar from "@/components/layout/Navbar";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
   title: "Cesar Paublini | Portfolio",
@@ -16,6 +17,7 @@ export default function RootLayout({
       <body className="pt-[64px] bg-black dark:bg-zinc-900">
         <Navbar />
         {children}
+        <Analytics />
       </body>
     </html>
   );
