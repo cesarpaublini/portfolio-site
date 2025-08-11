33a429fe3a1e6abea43314b015439c955f6b17dc
Cesar Paublini <cesar.paublini001@gmail.com>
2025-08-11 11:15:45 -0400

    Added contactsection import to the contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,10 @@
+import ContactSection from "@/components/sections/ContactSection";
 import Footer from "@/components/Footer";
 
 export default function ContactPage() {
   return (
     <div className="min-h-screen bg-black text-white">
-      <div className="p-10 text-center text-2xl">Contact Page</div>
+      <ContactSection />
       <Footer />
     </div>
   );
